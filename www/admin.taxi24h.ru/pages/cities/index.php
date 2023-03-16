6462353080f7d05935806cae2e095c8a6218aba9
stas yudin <info@evp-web.ru>
2023-03-16 10:27:36 +0300

    refactoring city main page

--- www/admin.taxi24h.ru/pages/cities/index.php
+++ www/admin.taxi24h.ru/pages/cities/index.php
@@ -10,7 +10,7 @@ include('../../components/cities/utils/functions-cities.php');
 
 
 checkAuthorization();
-$updateMain = updateMainCityByGet();
+$alert .= updateMainCityByGet();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,9 +32,9 @@ $updateMain = updateMainCityByGet();
             <? include('../../components/sidebar/sidebar.php') ?>
             <div class="content">
                 <div class="cities">
-                    <?echo $updateMain ?>
+                    <? echo $alert ?>
                     <form action="">
-                        <?echo drawCitiesTable(getAllCities(), $routs)?>
+                        <? drawCitiesTable() ?>
                         <input class="btn btn-primary" type="submit" value="Обновить Главный сайт">
                     </form>
                 </div>
