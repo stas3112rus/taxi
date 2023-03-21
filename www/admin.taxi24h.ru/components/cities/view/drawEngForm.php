b724eb87c5b5c208193afa4537c3eaccf2f16a31
stas yudin <info@evp-web.ru>
2023-03-21 13:11:35 +0300

    draw eng

---
+++ www/admin.taxi24h.ru/components/cities/view/drawEngForm.php
@@ -0,0 +1,21 @@
+<?
+function drawEngForm($city)
+{
+?>
+    <form action="" method="post">
+        <table class='table table-striped'>
+            <tbody>
+                <tr>
+                    <th scope='row' style='width: 200px;'>ENG</th>
+                    <td>
+                        <input type='text' class='form-control' placeholder='ENG' aria-label='ENG' aria-describedby='basic-addon1' name='eng' value='<? echo $city['eng'] ?? '' ?>' required>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <input type="hidden" name="id_city" value="<? echo $city['id_city'] ?>">
+        <input class="btn btn-primary" type="submit" value="Обновить ENG">
+    </form>
+<?
+}
---
+++ www/admin.taxi24h.ru/pages/cities/eng/index.php
@@ -0,0 +1,49 @@
+<?
+include('../../../../../src/data/mysql.php');
+include('../../../../../src/data/text-types/functions.php');
+include('../../../components/authorization/utils/functions.php');
+include('../../../constants/routs.php');
+include('../../../components/alerts/drawAlert.php');
+include('../../../../../src/data/cities/functions.php');
+include('../../../components/cities/view/drawEngForm.php');
+
+checkAuthorization();
+
+$city = getCityById($_GET['id']) ?? false;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="../../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../css/main.min.css">
+
+    <title>Редактировать город</title>
+</head>
+
+<body>
+    <section class="main">
+        <div class="main__wrapper">
+            <? include('../../../components/sidebar/sidebar.php') ?>
+            <div class="content">
+                <?
+                if ($city) {
+                    echo $alert ?>
+                    <h1><? echo "$city[im]" ?></h1>
+                    <? echo drawEngForm($city) ?>
+                <?
+                } else {
+                    echo drawAlert("Информация о городе отсутствует", "alert-danger");
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
