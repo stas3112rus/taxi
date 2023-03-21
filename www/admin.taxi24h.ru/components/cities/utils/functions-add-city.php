ff7238a15085fedfa8282c8202a131d7e210a04d
stas yudin <info@evp-web.ru>
2023-03-21 17:57:40 +0300

    fix 2

--- www/admin.taxi24h.ru/components/cities/utils/functions-add-city.php
+++ www/admin.taxi24h.ru/components/cities/utils/functions-add-city.php
@@ -1,90 +1,133 @@
 <?
+
 function createCity($city)
 {
-    // Создали запись в БД
-    $city['eng'] = mb_strtolower($city['eng']);
 
-    $createCityInDataBase = addCity($city);
-    if ($createCityInDataBase != "Ok")
-        return drawAlert($createCityInDataBase, "alert-danger");
+    $entriesToDataBase =  makeEntriesToDataBase($city);
+    if ($entriesToDataBase['status'] != 'Ok')
+        return cancelDataBaseAddCity($entriesToDataBase['status'], $city);
 
-    $city['id_city'] = getCityByEng($city['eng'])['id_city'];
-
-
-    $addTariffs = createTariffs($city['id_city'], $city['basic-city-id']);
-    if ($addTariffs != "Ok") {
-        deleteCityById($city['id_city']);
-        return drawAlert($addTariffs, "alert-danger");
-    }
+    $city['id_city'] = $entriesToDataBase['city']['id_city'];
 
     $deployNewDomain = deployNewDomain($city);
 
     if ($deployNewDomain != 'Ok') {
-        return $deployNewDomain;
+        return cancelFilesAddCity($deployNewDomain, $city);
     }
 
     return drawAlert("Город добавлен", "alert-success");
 }
 
+function makeEntriesToDataBase($city)
+{
+    $createCityInDataBase = addCity($city);
+    if ($createCityInDataBase != "Ok")
+        return [
+            'status' =>  $createCityInDataBase
+        ];
+
+    $city['id_city'] = getCityByEng($city['eng'])['id_city'];
+
+    $addTariffs = createTariffs($city['id_city'], $city['basic-city-id']);
+    if ($addTariffs != "Ok")
+        return [
+            'status' =>  $addTariffs
+        ];
+
+    return [
+        'status' => 'Ok',
+        'city' => $city
+    ];
+}
+
 function deployNewDomain($city)
 {
-    //Копируем статические файлы 
-    $newDomainPath = getFullPathToDomain($city);
-
     $domainFolder = copy_folder(
         getFullPathToStaticFiles(),
-        $newDomainPath
+        getFullPathToDomain($city)
     );
 
     if ($domainFolder != 'Ok') {
-        deleteFolder($newDomainPath);
-        deleteTariffsById($city['id_city']);
-        deleteCityById($city['id_city']);
-
-        return drawAlert($domainFolder, "alert-danger");
+        return $domainFolder;
     }
 
     $infoFile = deployInfoFile($city);
     if ($infoFile != 'Ok') {
-        return drawAlert($infoFile, "alert-danger");
+        return $infoFile;
     }
 
     $indexFile = deployIndexFile($city);
     if ($indexFile != 'Ok') {
-        return drawAlert($indexFile, "alert-danger");
+        return $indexFile;
     }
 
-    $directionsInsideCity = deployDirectionsAndTransfer($city);
-    if ($directionsInsideCity != "Ok")
-        return drawAlert($directionsInsideCity, "alert-danger");
-
-    $directionsOutsideCity = deployDirectionsAndTransfer($city, true);
-    if ($directionsOutsideCity != "Ok")
-        return drawAlert($directionsOutsideCity, "alert-danger");
-
-    $transferInsideCity = deployDirectionsAndTransfer($city, false, true);
-    if ($transferInsideCity != "Ok")
-        return drawAlert($transferInsideCity, "alert-danger");
-
-    $transferOutsideCity = deployDirectionsAndTransfer($city, true, true);
-    if ($transferOutsideCity != "Ok")
-        return drawAlert($transferOutsideCity, "alert-danger");
+    $deployDirectionAndTransfers = deployDirectionAndTransfers($city);
+    if ($deployDirectionAndTransfers != 'Ok') {
+        return $deployDirectionAndTransfers;
+    }
 
     $robotDeploy = deployRobot($city);
     if ($robotDeploy != "Ok")
-        return drawAlert($robotDeploy, "alert-danger");
+        return $robotDeploy;
 
     $sitemapDeploy = deploySitemaps();
     if ($sitemapDeploy != "Ok")
-        return drawAlert($sitemapDeploy, "alert-danger");
+        return $sitemapDeploy;
+
+    $htacces =  deployHtacces($city);
+    if ($htacces != "Ok")
+        return $htacces;
 
     return 'Ok';
 }
 
-function cancelDataBaseAddCity()
+function deployDirectionAndTransfers($city)
 {
+    $directionsInsideCity = deployDirectionsAndTransfer($city);
+    if ($directionsInsideCity != "Ok")
+        return $directionsInsideCity;
+
+    $directionsOutsideCity = deployDirectionsAndTransfer($city, true);
+    if ($directionsOutsideCity != "Ok")
+        return $directionsOutsideCity;
+
+    $transferInsideCity = deployDirectionsAndTransfer($city, false, true);
+    if ($transferInsideCity != "Ok")
+        return $transferInsideCity;
+
+    $transferOutsideCity = deployDirectionsAndTransfer($city, true, true);
+    if ($transferOutsideCity != "Ok")
+        return $transferOutsideCity;
+
+    return 'Ok';
 }
 
-function cancelFilesAddCity()
+function cancelDataBaseAddCity($message, $city)
 {
+    deleteTariffsById($city['id_city']);
+    deleteCityById($city['id_city']);
+    return drawAlert($message, "alert-danger");
+}
+
+function cancelFilesAddCity($message, $city)
+{
+    deleteTariffsById($city['id_city']);
+    deleteCityById($city['id_city']);
+
+    deleteFolder(getFullPathToDomain($city));
+    deleteDirectionsAndTransfers($city);
+
+    deploySitemaps();
+
+    return drawAlert($message, "alert-danger");
+}
+
+function deleteDirectionsAndTransfers($city_to)
+{
+    $cities_from = getAllWithoutOneCities($city_to['id_city']);
+
+    foreach ($cities_from as $city_from) {
+        deleteFolder(getFullPathTransferFolder($city_from, $city_to));
+        deleteFolder(getFullPathDirectionFolder($city_from, $city_to));
+    }
 }
