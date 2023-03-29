5c8e027261eca4eccffb3ec2e48966d865222de4
stas yudin <info@evp-web.ru>
2023-03-29 17:26:25 +0300

    fix text-page

--- src/deploy/text-for-deploy/index-and-info-texts.php
+++ src/deploy/text-for-deploy/index-and-info-texts.php
@@ -16,12 +16,12 @@ function getIndexTxt($is_main = false)
         ');
 }
 
-function getDirectionTxt($city_to_id, $is_main = false)
+function getDirectionTxt($city)
 {
-    $text_type = $is_main ?  2 : 4;
+    $text_type = $city['main_city'] ?  2 : 4;
 
     return ('<?
-    $CITY_TO_ID = ' . $city_to_id . ';
+    $CITY_TO_ID = ' . $city['id_city'] . ';
     $TEXT_TYPE_INDEX = ' . $text_type  . ';
     include ("' . getPathToInfoFile(1) . '");
     include ("' . getPathToMainFile(1) . '");
--- src/deploy/utils/deploy-directions-and-transfers.php
+++ src/deploy/utils/deploy-directions-and-transfers.php
@@ -37,7 +37,7 @@ function deployDirectionsAndTransfer($city_from, $reverse = false, $transfer = f
 function getDataForDirection($city_from, $city_to, $reverse)
 {
     return [
-        'txt' => getDirectionTxt($reverse ? $city_from['id_city'] :  $city_to['id_city']),
+        'txt' => getDirectionTxt($reverse ?  $city_to : $city_from),
         'dir_folder' => $reverse ?
             getFullPathDirectionFolder($city_to, $city_from) :
             getFullPathDirectionFolder($city_from, $city_to),
