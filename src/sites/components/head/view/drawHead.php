b95c37e4e286a69957311240f4e4059f7f1ff07b
stas yudin <info@evp-web.ru>
2023-03-29 14:21:51 +0300

    keywords

--- src/sites/components/head/view/drawHead.php
+++ src/sites/components/head/view/drawHead.php
@@ -8,6 +8,7 @@ function drawHead()
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <title><? echo $TXT["title"] ?></title>
     <meta name="description" content="<? echo $TXT["description"] ?>">
+    <meta name="keywords" content="<? echo $TXT["keywords"] ?>" />
     <link rel="shortcut icon" href="<? echo $CURRENT_SITE ?>favicon.png">
     <link rel="stylesheet" type="text/css" href="<? echo $CURRENT_SITE ?>css/bootstrap.min.css" />
     <link rel="stylesheet" type="text/css" href="<? echo $CURRENT_SITE ?>css/style.css?v=1.3" />
