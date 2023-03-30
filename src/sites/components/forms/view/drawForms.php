ec9b94dd002426a778f19722bd9508072eb463d0
stas yudin <info@evp-web.ru>
2023-03-30 11:45:03 +0300

    mainblock - 2

--- src/sites/components/forms/view/drawForms.php
+++ src/sites/components/forms/view/drawForms.php
@@ -4,150 +4,139 @@ function drawTopForm()
 ?>
     <form method="post" class="ajax-contact-form contact_form form-style" id="contact_form-1" action="mail.php">
         <div id="contact_body">
-            <label class="flatpickr" alt="Select Date and Time *">
-                <input class="input-field flatpickr" data-enabletime=true data-time_24hr=true data-timeFormat="H:i" name="date" type="text" placeholder="Выберите дату и время *">
-                <i class="fa fa-calendar form-icon" aria-hidden="true"></i>
-            </label>
-            <label for="subject">
-                <select name="subject-from" class="select-field">
-                    <option value="">Откуда</option>
-                    <option value="Симферополь">Симферополь (аэропорт - город)</option>
-                    <option value="Ай-Даниль">Ай-Даниль</option>
-                    <option value="Алупка">Алупка</option>
-                    <option value="Алушта">Алушта</option>
-                    <option value="Армянск">Армянск</option>
-                    <option value="Балаклава">Балаклава</option>
-                    <option value="Бахчисарай">Бахчисарай</option>
-                    <option value="Белогорск">Белогорск</option>
-                    <option value="Гаспра">Гаспра</option>
-                    <option value="Гурзуф">Гурзуф</option>
-                    <option value="Евпатория">Евпатория</option>
-                    <option value="Кастрополь">Кастрополь</option>
-                    <option value="Кацивели">Кацивели</option>
-                    <option value="Кача">Кача</option>
-                    <option value="Керчь">Керчь</option>
-                    <option value="Коктебель">Коктебель</option>
-                    <option value="Курпаты">Курпаты</option>
-                    <option value="Ласпи">Ласпи</option>
-                    <option value="Ливадия">Ливадия</option>
-                    <option value="Любимовка">Любимовка</option>
-                    <option value="Малореченское">Малореченское</option>
-                    <option value="Меллас">Меллас</option>
-                    <option value="Мисхор">Мисхор</option>
-                    <option value="Мрия">СКК Мрия Резорт</option>
-                    <option value="Николаевка">Николаевка</option>
-                    <option value="Новый Свет">Новый Свет</option>
-                    <option value="Оползневое">Оползневое (отель Мрия)</option>
-                    <option value="Орджоникидзе">Орджоникидзе</option>
-                    <option value="Орлиное">Орлиное</option>
-                    <option value="Парковое">Парковое</option>
-                    <option value="Партенит">Партенит</option>
-                    <option value="Понизовка">Понизовка</option>
-                    <option value="Поповка">Поповка (Казантип)</option>
-                    <option value="Приморский">Приморский (Феодосия)</option>
-                    <option value="Рыбачье">Рыбачье</option>
-                    <option value="Саки">Саки</option>
-                    <option value="Санаторное">Санаторное</option>
-                    <option value="Севастополь">Севастополь</option>
-                    <option value="Симеиз">Симеиз</option>
-                    <option value="Солнечногорское">Солнечногорское</option>
-                    <option value="Судак">Судак</option>
-                    <option value="Утес">Утес</option>
-                    <option value="Учкуевка">Учкуевка</option>
-                    <option value="Феодосия">Феодосия</option>
-                    <option value="Форос">Форос</option>
-                    <option value="Черноморское">Черноморское</option>
-                    <option value="Чонгар">Чонгар</option>
-                    <option value="Щелкино">Щелкино</option>
-                    <option value="Ялта">Ялта</option>
-                </select>
-            </label>
-            <label for="subject">
-                <select name="subject-to" class="select-field">
-                    <option value="">Куда</option>
-                    <option value="Симферополь">Симферополь (аэропорт - город)</option>
-                    <option value="Ай-Даниль">Ай-Даниль</option>
-                    <option value="Алупка">Алупка</option>
-                    <option value="Алушта">Алушта</option>
-                    <option value="Армянск">Армянск</option>
-                    <option value="Балаклава">Балаклава</option>
-                    <option value="Бахчисарай">Бахчисарай</option>
-                    <option value="Белогорск">Белогорск</option>
-                    <option value="Гаспра">Гаспра</option>
-                    <option value="Гурзуф">Гурзуф</option>
-                    <option value="Евпатория">Евпатория</option>
-                    <option value="Керчь">Керчь</option>
-                    <option value="Кастрополь">Кастрополь</option>
-                    <option value="Кацивели">Кацивели</option>
-                    <option value="Кача">Кача</option>
-                    <option value="Коктебель">Коктебель</option>
-                    <option value="Курпаты">Курпаты</option>
-                    <option value="Ласпи">Ласпи</option>
-                    <option value="Ливадия">Ливадия</option>
-                    <option value="Любимовка">Любимовка</option>
-                    <option value="Малореченское">Малореченское</option>
-                    <option value="Меллас">Меллас</option>
-                    <option value="Мисхор">Мисхор</option>
-                    <option value="Мрия">СКК Мрия Резорт</option>
-                    <option value="Николаевка">Николаевка</option>
-                    <option value="Новый Свет">Новый Свет</option>
-                    <option value="Оползневое">Оползневое (отель Мрия)</option>
-                    <option value="Орджоникидзе">Орджоникидзе</option>
-                    <option value="Орлиное">Орлиное</option>
-                    <option value="Парковое">Парковое</option>
-                    <option value="Партенит">Партенит</option>
-                    <option value="Понизовка">Понизовка</option>
-                    <option value="Поповка">Поповка (Казантип)</option>
-                    <option value="Приморский">Приморский (Феодосия)</option>
-                    <option value="Рыбачье">Рыбачье</option>
-                    <option value="Саки">Саки</option>
-                    <option value="Санаторное">Санаторное</option>
-                    <option value="Севастополь">Севастополь</option>
-                    <option value="Симеиз">Симеиз</option>
-                    <option value="Солнечногорское">Солнечногорское</option>
-                    <option value="Судак">Судак</option>
-                    <option value="Утес">Утес</option>
-                    <option value="Учкуевка">Учкуевка</option>
-                    <option value="Феодосия">Феодосия</option>
-                    <option value="Форос">Форос</option>
-                    <option value="Черноморское">Черноморское</option>
-                    <option value="Чонгар">Чонгар</option>
-                    <option value="Щелкино">Щелкино</option>
-                    <option value="Ялта">Ялта</option>
-                </select>
-            </label>
-            <label for="taxi" class="taxi">
-                <select name="taxi">
-                    <option value="">Выберите класс такси</option>
-                    <option value="Standart">Эконом</option>
-                    <option value="Komfort">Комфорт</option>
-                    <option value="Biznes">Бизнес</option>
-                    <option value="BUS-MINIVAN">Микроавтобус</option>
-                </select>
-            </label>
-            <label alt="Name *">
-                <input type="text" name="name" id="name" required class="input-field" placeholder="Имя *" />
-                <i class="fa fa-user form-icon" aria-hidden="true"></i>
-            </label>
-            <label>
-                <input type="text" name="phones" maxlength="19" required placeholder="Телефон *" class="tel-number-field long" />
-                <i class="fa fa-phone form-icon" aria-hidden="true"></i>
-            </label>
-            <label>
-                <input name="email" class="input-field" placeholder="E-mail" type="email">
-                <i class="fa fa-envelope-o form-icon" aria-hidden="true"></i>
-            </label>
-            <label for="field5">
-                <textarea name="message" id="message" class="textarea-field" placeholder="Ваше сообщение *"></textarea>
-                <i class="fa fa-commenting-o form-icon" aria-hidden="true"></i>
-            </label>
-            <label for="field6">
-                <input name="check" class="input-field" placeholder="1+1=" type="text">
-            </label>
-            <label>
-                <input type="submit" id="submit_btn" class="wbutton" value="ЗАБРОНИРОВАТЬ" />
-            </label>
+            <?
+            drawDatePicker();
+            drawSelectFromField();
+            drawSelectToField();
+            drawCLassTaxiField();
+            drawFieldName();
+            drawFieldPhone();
+            drawFieldEmail();
+            drawFieldMessage();
+            drawFieldCheck();
+            drawButtonSend();
+            ?>
         </div>
     </form>
+<?
+}
+
+function drawDatePicker()
+{
+?>
+    <label class="flatpickr" alt="Select Date and Time *">
+        <input class="input-field flatpickr" data-enabletime=true data-time_24hr=true data-timeFormat="H:i" name="date" type="text" placeholder="Выберите дату и время *">
+        <i class="fa fa-calendar form-icon" aria-hidden="true"></i>
+    </label>
+<?
+}
+
+function drawSelectFromField()
+{
+    global $CITY_FROM;
+?>
+    <label for="subject">
+        <select name="subject-from" class="select-field">
+            <option value="">Откуда</option>
+            <? drawCitiesOptions($CITY_FROM) ?>
+        </select>
+    </label>
+<?
+}
+
+function drawSelectToField()
+{
+    global $CITY_TO;
+
+?>
+    <label for="subject">
+        <select name="subject-from" class="select-field">
+            <option value="">Откуда</option>
+            <? drawCitiesOptions($CITY_TO) ?>
+        </select>
+    </label>
+<?
+}
+
+function drawCitiesOptions($city)
+{
+    global $ALL_CITIES;
+
+    foreach ($ALL_CITIES as $value) {
+
+        $select = "";
+
+        if ($city['id_city'] == $value['id_city'])
+            $select = "selected='selected'";
+
+        echo "<option value='$value[im]' $select>$value[im]</option>";
+    }
+}
+
+function drawCLassTaxiField()
+{
+?>
+    <label for="taxi" class="taxi">
+        <select name="taxi">
+            <option value="">Выберите класс такси</option>
+            <option value="Standart">Эконом</option>
+            <option value="Komfort">Комфорт</option>
+            <option value="Biznes">Бизнес</option>
+            <option value="BUS-MINIVAN">Микроавтобус</option>
+        </select>
+    </label>
+<?
+}
+
+function drawFieldName()
+{
+?>
+    <label alt="Name *">
+        <input type="text" name="name" id="name" required class="input-field" placeholder="Имя *" />
+        <i class="fa fa-user form-icon" aria-hidden="true"></i>
+    </label>
+<?
+}
+function drawFieldPhone()
+{
+?>
+    <label>
+        <input type="text" name="phones" maxlength="19" required placeholder="Телефон *" class="tel-number-field long" />
+        <i class="fa fa-phone form-icon" aria-hidden="true"></i>
+    </label>
+<?
+}
+function drawFieldEmail()
+{
+?>
+    <label>
+        <input name="email" class="input-field" placeholder="E-mail" type="email">
+        <i class="fa fa-envelope-o form-icon" aria-hidden="true"></i>
+    </label>
+<?
+}
+function drawFieldMessage()
+{
+?>
+    <label for="field5">
+        <textarea name="message" id="message" class="textarea-field" placeholder="Ваше сообщение *"></textarea>
+        <i class="fa fa-commenting-o form-icon" aria-hidden="true"></i>
+    </label>
+<?
+}
+function drawFieldCheck()
+{
+?>
+    <label for="field6">
+        <input name="check" class="input-field" placeholder="1+1=" type="text">
+    </label>
+<?
+}
+function drawButtonSend()
+{
+?>
+    <label>
+        <input type="submit" id="submit_btn" class="wbutton" value="ЗАБРОНИРОВАТЬ" />
+    </label>
 <?
 }
--- src/sites/components/mainBlock/view/drawMainBlock.php
+++ src/sites/components/mainBlock/view/drawMainBlock.php
@@ -1,24 +1,29 @@
 <?
 function drawMainBlock()
 {
+    global $TXT;
 ?>
     <section class="banner fix-background parallax" style="background-image:url(img/section__bg.jpg)">
         <div class="opacity-dark bg-black"></div>
         <div class="section">
             <div class="col-md-8 col-sm-12">
                 <div class="banner-text menuMenu scroll">
-                    <h1><?php echo $h1 ?></h1>
+                    <h1><?php echo strtoupper($TXT['h1']) ?></h1>
                     <p class="text-shadow">
-                        ГАРАНТИРОВАННО В ТОЧНОЕ ВРЕМЯ - ПРИЕДЕМ В ЛЮБУЮ ТОЧКУ <?php echo $city_rod_zaglav ?> И ВСТРЕТИМ В АЭРОПОРТУ
+                        <? echo strtoupper($TXT['subtitle_main']) ?>
                     </p>
                     <a href="#class-taxi">
-                        <button class="wbutton-bordered white">УЗНАТЬ СТОИМОСТЬ</button>
+                        <button class="wbutton-bordered white">
+                            <? echo strtoupper($TXT['button_main']) ?>
+                        </button>
                     </a>
                 </div>
             </div>
             <div class="col-md-4 col-sm-12">
                 <div class="sidebar-search">
-                    <h6>ЗАБРОНИРОВАТЬ ТАКСИ В <?php echo $city_vin_zaglav ?> СЕЙЧАС</h6>
+                    <h6>
+                        <? echo strtoupper(strtoupper($TXT['top_form_title'])) ?>
+                    </h6>
                     <hr class="form-line">
                     <div class="signup-box-body">
                         <? drawTopForm() ?>
