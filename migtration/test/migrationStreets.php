e60409e465281cdcf26ca4148bc8702af22c4e32
stas yudin <info@evp-web.ru>
2023-04-01 12:32:50 +0300

    migration streets

---
+++ migtration/test/migrationStreets.php
@@ -0,0 +1,15 @@
+<?
+include('mysql.php');
+$sql = mysql_query("SELECT eng 
+    FROM sklonenie");
+while ($dp = mysql_fetch_object($sql)) {
+    $eng = $dp->eng;
+
+    $src = "../" . $eng . ".aeroport-simferopol.taxi/info.php";
+
+    include($src);
+    $uliza = iconv('windows-1251', 'utf-8', $uliza);
+    if (!empty($uliza)) {
+        echo "UPDATE `cities` SET `street`='$uliza' WHERE `eng` = '$eng'" . "<br>";
+    }
+}
