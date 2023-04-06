3015c6a793297d4c7a48c04e6b2bec3beb0b2c8a
stas yudin <info@evp-web.ru>
2023-04-06 13:03:31 +0300

    migration yandex verification

---
+++ www/admin.sydin.ru/pages/migration/yandex-migration.php
@@ -0,0 +1,25 @@
+<?
+include('../../../../src/data/mysql.php');
+include('../../../../migtration/migration_yandex.php');
+include('../../../../src/data/cities/functions.php');
+include('../../../../src/data/defaults/functions.php');
+include('../../../../src/utils/functions.php');
+include('../../../../src/deploy/main.php');
+
+checkAuthorization();
+
+$cities = getAllCities();
+
+$create = [];
+
+foreach (getAllCities() as $city) {
+    $url = getMainUrl($city['eng'], $city['main_city']);
+
+    $yandex_str = getYandexVerification($url);
+
+    if ($yandex_str) {
+        echo "INSERT INTO `widgets`( `city_ref`, `widget_type_ref`, `widget_code`) VALUES ('$city[id_city]','25','$yandex_str')" . "<br>";
+    }
+}
+
+print_r($create);
