62658e0ee1e749248db1ada5a890c6199a7b262f
stas yudin <info@evp-web.ru>
2023-04-07 11:08:47 +0300

    head and scripts

--- src/sites/components/head/view/drawHead.php
+++ src/sites/components/head/view/drawHead.php
@@ -1,34 +1,23 @@
 <?
 function drawHead()
 {
-    global $TXT, $LEVEL, $DEFAULT, $TEXT_TYPE_INDEX;
+    global $TXT, $LEVEL;
 ?>
-    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- ==============================================
-    Title and Meta Tags
-    =============================================== -->
-    <title><?php echo $TXT['title'] ?></title>
-
-    <meta name="description" content="<?php echo $TXT['description'] ?>">
-    <? if ($TEXT_TYPE_INDEX == 1 || $TEXT_TYPE_INDEX == 3) { ?>
-        <meta property="og:title" content="<?php echo $TXT['title'] ?>">
-        <meta property="og:description" content="<?php echo $TXT['description'] ?>">
-        <meta property="og:image" content="<? echo $LEVEL ?>img/master/logo.png" />
-        <meta property="og:url" content="<? echo $LEVEL ?>" />
-        <meta property="og:site_name" content="<?php echo $DEFAULT['domain'] ?>" />
-        <meta property="og:locale" content="ru_RU" />
-    <? } ?>
-    <meta name="keywords" content="<? echo $TXT['keywords'] ?>">
-    <link rel="shortcut icon" href="<? echo $LEVEL ?>img/favicon.png">
-    <link href="https://fonts.googleapis.com/css?family=Oswald:400,700&amp;subset=cyrillic" rel="stylesheet">
-    <link rel="stylesheet" href="<? echo $LEVEL ?>css/bootstrap.min.css">
-    <link rel="stylesheet" href="<? echo $LEVEL ?>css/styles.css">
-    <link rel="stylesheet" href="<? echo $LEVEL ?>css/font-awesome.min.css">
-    <link rel='stylesheet' href='<? echo $LEVEL ?>css/animate.min.css'>
-    <link rel='stylesheet' href='<? echo $LEVEL ?>css/slick.min.css'>
-    <link rel='stylesheet' href='<? echo $LEVEL ?>js/flatpickr/dist/flatpickr.min.css'>
-    <link rel="stylesheet" href="<? echo $LEVEL ?>css/owl.carousel.min.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="<? echo $LEVEL ?>favicon.png" rel="shortcut icon" type="image/png" />
+    <title><? echo $TXT['title'] ?></title>
+    <meta name="description" content="<? echo $TXT['description'] ?>" />
+    <meta name="keywords" content="<? echo $TXT['keywords'] ?>" />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/fonts/font-awesome/css/font-awesome.min.css" type='text/css'>
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/style.css" type='text/css' />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/animate.css" type='text/css' />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/owl.carousel.css" type='text/css' />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/fixed-nav.css">
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/fonts/car-serive-icon.css" type='text/css'>
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/magnific-popup.css" type='text/css' />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/twentytwenty.css">
+    <link rel="stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/js/flatpickr/flatpickr.css">
+    <link rel=" stylesheet" href="<? echo $LEVEL ?>builder/disc/elements/css/color/yellow.css" />
 <?
 }
--- src/sites/components/scripts/view/drawScripts.php
+++ src/sites/components/scripts/view/drawScripts.php
@@ -4,24 +4,24 @@ function drawScripts()
     global $LEVEL;
 
 ?>
-    <a href="#0" class="cd-top">Top</a>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js"></script>
-
-    <script src="<? echo $LEVEL ?>js/top.js"></script>
-    <script src='<? echo $LEVEL ?>js/flickity.pkgd.min.js'></script>
-    <script src="<? echo $LEVEL ?>js/testimonials.js"></script>
-    <script src="<? echo $LEVEL ?>js/counter.js"></script>
-    <script src="<? echo $LEVEL ?>js/jquery.waypoints.min.js"></script>
-    <script src="<? echo $LEVEL ?>js/jquery.counterup.js"></script>
-    <script src="<? echo $LEVEL ?>js/bootstrap.min.js"></script>
-    <script src="<? echo $LEVEL ?>js/contact.js"></script>
-    <script src="<? echo $LEVEL ?>js/validator.js"></script>
-    <script src="<? echo $LEVEL ?>js/navbar.js"></script>
-    <script src="<? echo $LEVEL ?>js/loader.js"></script>
-    <script src='<? echo $LEVEL ?>js/slick.min.js'></script>
-    <script src="<? echo $LEVEL ?>js/car.slider.js"></script>
-    <script src="<? echo $LEVEL ?>js/jquery.magnific-popup.min.js"></script>
-    <script src="<? echo $LEVEL ?>js/flatpickr/flatpickr.js"></script>
-    <script src="<? echo $LEVEL ?>js/common.js"></script>
+    <a href="#top">
+        <p id="back-top"><i class="fa fa-chevron-up"></i> </p>
+    </a>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="builder/disc/elements/js/jquery.magnific-popup.js"></script>
+    <script src="builder/disc/elements/js/waypoints.min.js"></script>
+    <script src="builder/disc/elements/js/jquery.counterup.js"></script>
+    <script src="builder/disc/elements/js/responsive-nav.js"></script>
+    <script src="builder/disc/elements/js/fastclick.js"></script>
+    <script type="text/javascript" src="builder/disc/elements/js/owl.carousel.js"></script>
+    <script src="builder/disc/elements/js/flatpickr/flatpickr.js"></script>
+    <script src="builder/disc/elements/js/flatpickr/flatpickr.js"></script>
+    <script src="builder/disc/elements/js/jquery.singlePageNav.js"></script>
+    <script src="builder/disc/elements/js/jquery.twentytwenty.js"></script>
+    <script src="builder/disc/elements/js/jquery.event.move.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/stickynavbar.js/1.3.2/jquery.stickyNavbar.min.js"></script>
+    <script src="maine-js/jquery.cookie.js"></script>
+    <script src="maine-js/main.js"></script>
 <?
 }
