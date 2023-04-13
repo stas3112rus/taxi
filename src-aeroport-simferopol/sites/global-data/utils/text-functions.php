5cfb7f29f99e1f7ae3b29adaaf6d7ea91e5cc407
stas yudin <info@evp-web.ru>
2023-04-13 12:19:03 +0300

    title price

--- src-aeroport-simferopol/sites/global-data/utils/text-functions.php
+++ src-aeroport-simferopol/sites/global-data/utils/text-functions.php
@@ -16,14 +16,19 @@ function getTXT()
 
 function upgradeTextLine($textLine)
 {
-    global $DEFAULT, $MAIN_SITE, $CURRENT_SITE, $DIRECTION_URL;
+    global $DEFAULT, $MAIN_SITE, $CURRENT_SITE, $DIRECTION_URL, $YEAR, $STOP_DISCOUNT_DAY;
 
     $textLine = replacePadeg($textLine);
     $textLine = str_replace("[min_price]", getMinPrice(), $textLine);
+    $textLine = str_replace("[title_price]", getTitlePrice(), $textLine);
     $textLine = str_replace("[phone]", getFullPhone(), $textLine);
     $textLine = str_replace("[domen]", $DEFAULT['domain'], $textLine);
     $textLine = str_replace("[main_url]", $MAIN_SITE, $textLine);
     $textLine = str_replace("[domain_url]", $CURRENT_SITE, $textLine);
+    $textLine = str_replace("[year]", $YEAR, $textLine);
+    $textLine = str_replace("[discount_day]", $STOP_DISCOUNT_DAY, $textLine);
+
+
 
     if ($DIRECTION_URL) {
         $textLine =  str_replace("[direction_url]", $DIRECTION_URL, $textLine);
@@ -65,4 +70,11 @@ function getMinPrice()
     global $TARIFF, $DEFAULT;
 
     return round($TARIFF['economy'] * $DEFAULT['min_price_percent'] / 100);
+}
+
+function getTitlePrice()
+{
+    global $TARIFF, $DEFAULT;
+
+    return round($TARIFF['economy'] * $DEFAULT['discount_title'] / 100);
 }
