4a8a9b6e951a898ba8e36b08c01981389b38c402
stas yudin <info@evp-web.ru>
2023-04-19 16:33:22 +0300

    head and scripts

--- src/sites/components/head/view/drawHead.php
+++ src/sites/components/head/view/drawHead.php
@@ -4,35 +4,24 @@ function drawHead()
 {
     global $TXT, $LEVEL;
 ?>
-    <meta charset="UTF-8" />
-    <title><? echo updateTitle($TXT['title']) ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<? echo $TXT['description'] ?>">
+    <link rel="icon" type="png" href="<? echo $LEVEL ?>favicon.png">
+    <title><? echo $TXT['title'] ?></title>
     <meta name="keywords" content="<? echo $TXT['keywords'] ?>">
-    <meta name="Resource-type" content="Document" />
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/bootstrap.min.css">
-    <link rel="shortcut icon" href="<? echo $LEVEL ?>favicon.png" type="image/png">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/themify-icons.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/jarallax.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/owl.carousel.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/owl.theme.default.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/slick.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/slick-theme.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/swiper.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/style.css">
-    <link rel="stylesheet" href="<? echo $LEVEL ?>assets/js/flatpickr/flatpickr.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/reset.css">
-    <link rel="stylesheet" type="text/css" href="<? echo $LEVEL ?>assets/css/responsive.css">
-<?
-}
+    <link href="<? echo $LEVEL ?>assets/css/bootstrap-grid.css" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/font-awesome.css" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/swiper.css" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/swipebox.css" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/zoomslider.css" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/style.css?03" rel="stylesheet">
+    <link href="<? echo $LEVEL ?>assets/css/style2.css?03" rel="stylesheet">
 
-function updateTitle($str)
-{
-    $str = mb_strtoupper($str);
-    return str_replace('РУБ.', 'руб.', $str);
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:700,800|Open+Sans:400,600,700&amp;subset=cyrillic">
+    <link rel="stylesheet" href="<? echo $LEVEL ?>assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="<? echo $LEVEL ?>assets/js/flatpickr/flatpickr.css">
+<?
 }
--- src/sites/components/scripts/view/drawScripts.php
+++ src/sites/components/scripts/view/drawScripts.php
@@ -4,19 +4,21 @@ function drawScripts()
     global $LEVEL;
 
 ?>
-    <a class="btn btn-lg scrollup"><i class="fa fa-arrow-up"></i></a>
 
-    <script src="<? echo $LEVEL ?>assets/js/jquery.2.2.3.min.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/bootstrap.min.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/modernizr-custom.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/scrollreveal.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/slick.min.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/swiper.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/jarallax.min.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/jquery.counterup.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/waypoints.min.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<? echo $LEVEL ?>assets/js/modernizr-2.6.2.min.js"></script>
+    <script src="<? echo $LEVEL ?>assets/js/jquery.min.js"></script>
+    <script src="<? echo $LEVEL ?>assets/js/jquery.magnific-popup.js"></script>
     <script src="<? echo $LEVEL ?>assets/js/flatpickr/flatpickr.js"></script>
-    <script src="<? echo $LEVEL ?>assets/js/script.js"></script>
+    <script src="<? echo $LEVEL ?>assets/js/plugins.min.js"></script>
+    <script>
+        flatpickr(".flatpickr", {
+            enableTime: true,
+            disableMobile: true,
+
+        });
+    </script>
+    <script src="<? echo $LEVEL ?>assets/js/scripts.js"></script>
+
+
 <?
 }
