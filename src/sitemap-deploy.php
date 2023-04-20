b2215d4d55a5b4ebca807512efbbddaf842e9e56
stas yudin <info@evp-web.ru>
2023-04-20 15:03:01 +0300

    sitemap

--- src/sitemap-deploy.php
+++ src/sitemap-deploy.php
@@ -24,8 +24,9 @@ $LEVEL = '';
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="png" href="favicon.png">
         <title>Карта сайта <?php echo $CURRENT_SITE ?></title>
-        <meta name="description" content="<?php echo $description ?>">
+        <meta name="description" content="">
         <meta name="keywords" content="">
+        <meta name="yandex-verification" content="9851373f16026a3b" />
         <link href="assets\css\bootstrap-grid.css" rel="stylesheet">
         <link href="assets\css\font-awesome.css" rel="stylesheet">
         <link href="assets\css\swiper.css" rel="stylesheet">
@@ -33,9 +34,7 @@ $LEVEL = '';
         <link href="assets\css\zoomslider.css" rel="stylesheet">
         <link href="assets\css\style.css?03" rel="stylesheet">
         <link href="assets\css\style2.css?03" rel="stylesheet">
-
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-
         <link href="https://fonts.googleapis.com/css?family=Fira+Sans+Condensed:700,800|Open+Sans:400,600,700&amp;subset=cyrillic" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/magnific-popup.css" type='text/css' />
         <link rel="stylesheet" href="assets/js/flatpickr/flatpickr.css">
