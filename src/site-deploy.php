807a0ad83ac12265b8e5d2a4871cf70336653cc5
stas yudin <info@evp-web.ru>
2023-05-02 10:46:05 +0300

    advantages

--- src/site-deploy.php
+++ src/site-deploy.php
@@ -34,6 +34,7 @@ include('sites/components/main.php');
         drawNavBar();
         drawMainBlock();
         drawTextBlock();
+        drawAdvantages();
         ?>
     </div>
     <?
--- src/sites/components/main.php
+++ src/sites/components/main.php
@@ -9,3 +9,4 @@ include('preloader/view/drawPreloader.php');
 include('navBar/view/drawNavBar.php');
 include('mainBlock/view/drawMainBlock.php');
 include('textBlock/view/drawTextBlock.php');
+include('advantages/view/drawAdvantages.php');
