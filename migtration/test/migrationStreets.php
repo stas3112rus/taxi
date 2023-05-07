66451411dbcbad4075ecb96e0796b487d09a050e
stas yudin <info@evp-web.ru>
2023-05-07 12:17:29 +0300

    migration streets

--- migtration/test/migrationStreets.php
+++ migtration/test/migrationStreets.php
@@ -5,7 +5,7 @@ $sql = mysql_query("SELECT eng
 while ($dp = mysql_fetch_object($sql)) {
     $eng = $dp->eng;
 
-    $src = "../" . $eng . ".feodosiya.taxi/info.php";
+    $src = "../" . $eng . ".kerch24.taxi/info.php";
 
     include($src);
     $uliza = iconv('windows-1251', 'utf-8', $uliza);
