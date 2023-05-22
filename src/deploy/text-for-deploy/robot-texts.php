866f617f8e1b6306383588ea8a55e4fbc8ec782c
stas yudin <info@evp-web.ru>
2023-05-22 09:58:17 +0300

    disallow get request robots.txt

--- src/deploy/text-for-deploy/robot-texts.php
+++ src/deploy/text-for-deploy/robot-texts.php
@@ -10,6 +10,7 @@ function getRobotTxt($city)
     return ('User-agent: *
 Disallow: /' . $city['eng'] . '
 Disallow: /transfer-' . $city['eng'] . '
+Disallow: /*?*
 Host: '.getMainUrl($city['eng'], $city['main_city']).' 
 Sitemap: ' . getUrlToSitemap($city) . '
 ' . $allUrls
