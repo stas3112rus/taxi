4fa59bcba574e80fc657d790f2d1ced298cde2de
stas yudin <info@evp-web.ru>
2023-05-26 09:53:32 +0300

    routes api

--- www/admin.sydin.ru/constants/sites-api-routes.php
+++ www/admin.sydin.ru/constants/sites-api-routes.php
@@ -5,6 +5,51 @@ $SITES_API_ROUTES = array(
         "notPublished" => "http://admin.taxi24h.ru/api/not-published/",
         "tariffs" => "http://admin.taxi24h.ru/api/tariffs/",
     ),
+        "aeroport-simferopol.taxi" => array(
+        "cities" => "http://admin.aeroport-simferopol.taxi/api/cities/",
+        "notPublished" => "http://admin.aeroport-simferopol.taxi/api/not-published/",
+        "tariffs" => "http://admin.aeroport-simferopol.taxi/api/tariffs/",
+    ),
+        "taxi-krim.com" => array(
+        "cities" => "http://admin.taxi-krim.com/api/cities/",
+        "notPublished" => "http://admin.taxi-krim.com/api/not-published/",
+        "tariffs" => "http://admin.taxi-krim.com/api/tariffs/",
+    ),
+        "simferopol-aeroport.taxi" => array(
+        "cities" => "http://admin.simferopol-aeroport.taxi/api/cities/",
+        "notPublished" => "http://admin.simferopol-aeroport.taxi/api/not-published/",
+        "tariffs" => "http://admin.simferopol-aeroport.taxi/api/tariffs/",
+    ),
+        "taksi24.taxi" => array(
+        "cities" => "http://admin.taksi24.taxi/api/cities/",
+        "notPublished" => "http://admin.taksi24.taxi/api/not-published/",
+        "tariffs" => "http://admin.taksi24.taxi/api/tariffs/",
+    ),
+        "yalta-24.taxi" => array(
+        "cities" => "http://admin.yalta-24.taxi/api/cities/",
+        "notPublished" => "http://admin.yalta-24.taxi/api/not-published/",
+        "tariffs" => "http://admin.yalta-24.taxi/api/tariffs/",
+    ),
+        "sevastopol24.taxi" => array(
+        "cities" => "http://admin.sevastopol24.taxi/api/cities/",
+        "notPublished" => "http://admin.sevastopol24.taxi/api/not-published/",
+        "tariffs" => "http://admin.sevastopol24.taxi/api/tariffs/",
+    ),
+        "alushta24.taxi" => array(
+        "cities" => "http://admin.alushta24.taxi/api/cities/",
+        "notPublished" => "http://admin.alushta24.taxi/api/not-published/",
+        "tariffs" => "http://admin.alushta24.taxi/api/tariffs/",
+    ),
+        "feodosiya.taxi" => array(
+        "cities" => "http://admin.feodosiya.taxi/api/cities/",
+        "notPublished" => "http://admin.feodosiya.taxi/api/not-published/",
+        "tariffs" => "http://admin.feodosiya.taxi/api/tariffs/",
+    ),
+        "kerch24.taxi" => array(
+        "cities" => "http://admin.kerch24.taxi/api/cities/",
+        "notPublished" => "http://admin.kerch24.taxi/api/not-published/",
+        "tariffs" => "http://admin.kerch24.taxi/api/tariffs/",
+    ),
     "sydin.ru" => array(
         "cities" => "http://admin.sydin.ru/api/cities/",
         "notPublished" => "http://admin.sydin.ru/api/not-published/",
